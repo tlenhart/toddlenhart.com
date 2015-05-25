6863e92fb09d749de70bd8650b144e4651de61f1
Todd <toddlenhart@gmail.com>
2015-05-25 15:42:39 -0600

    Live reload now works when running the 'gulp' task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,26 @@
 /**
  * Created by todd on 5/23/2015.
+ * gulpfile for toddlenhart.com
  */
+
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+
+
+gulp.task('connect', function(){
+  connect.server({
+    root: 'src/client',
+    livereload: true
+  });
+});
+
+gulp.task('html', function() {
+  gulp.src('./src/client/*.html')
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['./src/client/*.html'], ['html']);
+});
+
+gulp.task('default', ['connect', 'watch']);
